6b9a2caf50cdf0097965d2b8ce4583cfacdf8b40
hawnzug <hawnzug@gmail.com>
2015-11-20 10:51:29 +0800

    add probability

--- compare/trie_dict.py
+++ compare/trie_dict.py
@@ -1,11 +1,11 @@
 trie = {}
-def trie_add(word):
+def trie_add(word, prob):
     ref = trie
     for char in word:
         if char not in ref:
             ref[char] = {}
         ref = ref[char]
-    ref[''] = 1
+    ref[''] = prob
 
 
 def trie_search(word):
@@ -19,11 +19,12 @@ def trie_search(word):
 
 f = open('dict.txt', 'r')
 for line in f.readlines():
-    word = line.split()[0]
-    trie_add(word)
+    word, prob = line.split()[:2]
+    trie_add(word, prob)
+print(trie)
 
-print(trie_search('哈'))
-print(trie_search('妈'))
-print(trie_search('呵呵'))
-print(trie_search('整数'))
-print(trie_search('电脑'))
+#print(trie_search('哈'))
+#print(trie_search('妈'))
+#print(trie_search('呵呵'))
+#print(trie_search('整数'))
+#print(trie_search('电脑'))
